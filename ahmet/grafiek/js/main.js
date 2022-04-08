28f563a10ee597af3e294a688a49967d0804c974
AhmetAMA <33016@ma-web.nl>
2022-04-08 15:56:55 +0200

    grafiek verbeterd

--- ahmet/grafiek/js/main.js
+++ ahmet/grafiek/js/main.js
@@ -1,6 +1,6 @@
 var KWH = [6.3, 12.1, 10.4, 9.1, 8.7, 11.2, 7.9, 9.4];
 var week = ['week1', 'week2', 'week3', 'week4', 'week5', 'week6', 'week7', 'week8'];
-var cntxt = document.getElementById('Halil_Chart');
+var cntxt = document.getElementById('Ahmet_Chart');
 
 var myChart = new Chart(cntxt, {
     type: 'pie',
@@ -11,14 +11,14 @@ var myChart = new Chart(cntxt, {
                 label: 'Water verbruik per week in liter',
                 data: KWH,
                 backgroundColor: [
-                    "#99fcff",
-                    "#77fbff",
-                    "#55faff",
                     "#33f9ff",
                     "#11f8ff",
+                    "#22f8ff",
                     "#00e7ee",
+                    "#00f7ff",
                     "#00c6cc",
-                    "#00a5aa",
+                    "#00d6dd",
+                    "#00b5bb",
                 ],
                 borderColor: "black",
                 borderWidth: 1,
