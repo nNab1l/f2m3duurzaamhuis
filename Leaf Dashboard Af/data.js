1bca3773f4ae37a2d3304785cab71b2bd11d0d3f
calvin-heeres <34248@ma-web.nl>
2022-04-13 09:30:23 +0200

    Opdracht klaar

---
+++ Leaf Dashboard Af/data.js
@@ -0,0 +1,35 @@
+"use strict"
+        const timeDelay = 400; // time delay refresh data
+        let refreshTimer = window.setInterval(renderData, timeDelay); // timer data opvragen van server
+        
+        // jouw persoonlijke URL
+        const mijnDataURL = "https://data.softwaredeveloper.amsterdam/api/device/c7f40c1b/latest"; 
+        
+        const dataDiv = document.getElementById("dataDiv"); // hier komt de data
+        
+        async function getSensorData() {
+            let url = mijnDataURL;
+            try {
+                let response = await fetch(url);
+                return await response.json();
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        
+        async function renderData() {
+                let measurementRaw = await getSensorData();
+                let measurement = measurementRaw.data[0];
+                console.log(measurement);
+
+                 // Nieuwe Javascript date maken met de de datum en tijd van de meting
+                 let datum =  new Date(measurement.datum);
+               
+                 // De meting gegevens wegschrijven naar de div                
+                 dataDiv.innerHTML = `<h3></h3><p>Parameters SilverBullet:
+                    </p><p>Temperatuur: ${measurement.value1}
+                    </p><p> Vochtigheid: ${measurement.value2}
+                    </p><p> Lichtintensiteit: ${measurement.value3}</p>`;
+            }
+            
+        renderData(); // start immediately
---
+++ Leaf Dashboard Af/main.js
@@ -0,0 +1,222 @@
+link = "http://api.openweathermap.org/data/2.5/weather?q=Amsterdam&APPID=d09bd6a00bb40655fa75dc89a1f1b806&units=metric&lang=nl";
+var request = new XMLHttpRequest();
+request.open('GET',link,true);
+request.onload = function(){
+    var obj = JSON.parse(this.response);
+    let milliSecondsSindsEpoch = obj.sys.sunrise * 1000;
+    let milliSecondsSindsEpoch1 = obj.sys.sunset * 1000;
+    datum = new Date(milliSecondsSindsEpoch);
+    datum2 = new Date(milliSecondsSindsEpoch1);
+    console.log(obj);
+    document.getElementById('location').innerHTML = obj.name;
+    document.getElementById('weather').innerHTML = obj.weather[0].description;
+    document.getElementById('temp').innerHTML = obj.main.temp + "°C";
+    document.getElementById('sunset').innerHTML = "zon onder  " + (datum2.toLocaleString());;
+    document.getElementById('sunrise').innerHTML = "zon op  " + (datum.toLocaleString());;
+}
+
+if(request.status==200){
+    console.log("ERROR");
+}
+request.send();
+let datum2;
+const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Amsterdam&APPID=d09bd6a00bb40655fa75dc89a1f1b806&units=metric';
+fetch(URL)
+  .then(response => response.json())
+  .then(weather => {
+    console.log(weather);
+    let milliSecondsSindsEpoch = weather.sys.sunrise * 1000;
+    datum2 = new Date(milliSecondsSindsEpoch);
+    console.log(datum2.toLocaleString());
+  });
+
+let datum1;
+const URL1 = 'http://api.openweathermap.org/data/2.5/weather?q=Amsterdam&APPID=d09bd6a00bb40655fa75dc89a1f1b806&units=metric';
+fetch(URL)
+  .then(response => response.json())
+  .then(weather => {
+    console.log(weather);
+    let milliSecondsSindsEpoch1 = weather.sys.sunset * 1000;
+    datum1 = new Date(milliSecondsSindsEpoch1);
+    console.log(datum.toLocaleString());
+  });
+
+
+
+var ctx = document.getElementById('myChart');
+var stars = [1240.60, 1248.41, 1249.16, 1249.95, 1253.67];
+var frameworks = ['week 1', 'week 2', 'week 3', 'week 4', 'week 5'];
+  var myChart = new Chart(ctx, {
+       type: 'bar',
+       data:  {
+       labels: frameworks,     
+       datasets: [{ 
+           label: 'Waterverbruik Maart per m³',
+           borderRadius: 0,  
+           data: stars,
+           backgroundColor: [ 
+            "#94c49456", 
+            "#94c49456",
+            "#94c49456", 
+            "#94c49456", 
+            "#94c49456" 
+            ],
+            
+            borderColor: [  
+                "#94c494", 
+                "#94c494", 
+                "#94c494", 
+                "#94c494",
+                "#94c494", 
+            ],
+
+            barPercentage: 0.3,
+            borderWidth: 02,
+            
+          
+          
+        
+        }]
+    }
+
+    
+ }
+ )
+ 
+
+var ctx = document.getElementById('myChart1');
+var stars = [13518, 13522, 13532, 13537, 13542, 13548, 13552];
+var frameworks = ['Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag'];
+  var myChart = new Chart(ctx, {
+       type: 'doughnut',
+       data:  {
+       labels: frameworks,     
+       datasets: [{ 
+           label: 'Water',
+           borderRadius: 0,  
+           data: stars,
+           backgroundColor: [ 
+            "#94c49456", 
+            "#94c49456",
+            "#94c49456", 
+            "#94c49456", 
+            "#94c49456" 
+            ],
+
+            
+
+           
+            
+            borderColor: [  
+                "#94c494", 
+                "#94c494", 
+                "#94c494", 
+                "#94c494",
+                "#94c494", 
+            ],
+
+            barPercentage: 0.1,
+            borderWidth: 02,
+           
+            
+          
+          
+        
+        }]
+    }
+
+    
+ }
+ )
+
+
+
+ var ctx = document.getElementById('myChart2');
+var stars = [1325.143, 1327.889, 1332.413, 1334.461, 1337.461];
+var frameworks = ['week 1', 'week 2', 'week 3', 'week 4', 'week 5'];
+  var myChart = new Chart(ctx, {
+       type: 'line',
+       data:  {
+       labels: frameworks,     
+       datasets: [{ 
+           label: 'gasverbruik maart in m³',
+           borderRadius: 0,
+             
+           data: stars,
+           backgroundColor: [ 
+            "#94c49456", 
+            "#94c49456",
+            "#94c49456", 
+            "#94c49456", 
+            "#94c49456" 
+            ],
+
+            fill: true,
+            pointRadius: 2,
+
+
+            
+               
+                  
+               
+
+          
+            
+
+            
+
+           
+            
+            borderColor: [  
+                "#94c494", 
+                "#94c494", 
+                "#94c494", 
+                "#94c494",
+                "#94c494", 
+            ],
+
+            barPercentage: 0.1,
+            borderWidth: 02,
+
+
+            
+           
+            
+          
+          
+        
+        }]
+    }
+
+    
+ }
+ )
+
+ 
+
+ Date.UTC()
+
+
+ function startTime() {
+     const today = new Date();
+     let h = today.getHours();
+     let m = today.getMinutes();
+     let s = today.getSeconds();
+     m = checkTime(m);
+     s = checkTime(s);
+     document.getElementById('time').innerHTML =  h + ":" + m + ":" + s;
+     setTimeout(startTime, 1000);
+   }
+   
+   function checkTime(i) {
+     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+     return i;
+   }
+ 
+ let today = new Date().toISOString().slice(0, 10)
+
+
+
+ 
+ 
+ 
